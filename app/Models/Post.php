0ed4420b46d916b9a39993f69958141b28add8c2
Moonclood <makaroninproduction@gmail.com>
2023-08-29 19:05:37 +0300

    Типизация

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,20 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
-    public function user()
+    public function user(): BelongsTo|Builder
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function category()
+    public function category(): BelongsTo|Builder
     {
         return $this->belongsTo(Category::class, 'category_id');
     }
+    
+    
 }
