eb45c76fb4df5bfba395e009ef5afbdae7769aed
Moonclood <makaroninproduction@gmail.com>
2023-08-29 19:14:58 +0300

    Типизация

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
 
-    public function posts(): HasMany
+    public function posts(): HasMany | Builder
     {
         return $this->hasMany(Post::class, 'category_id', 'id');
     }
